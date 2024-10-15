c148c6f75c969a563e9d09750abdb1c53e75574f
Naruto-79 <rurrefath@gmail.com>
2024-10-15 10:57:38 +0600

    make product image clickable

--- app/[category]/page.tsx
+++ app/[category]/page.tsx
@@ -39,15 +39,17 @@ export default async function CategoryPage({
         <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
           {data.map((product) => (
             <div key={product._id} className="group relative">
-              <div className="aspect-square w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:h-80">
-                <Image
-                  src={product.imageUrl}
-                  alt="Product image"
-                  className="w-full h-full object-cover object-center lg:h-full lg:w-full"
-                  width={300}
-                  height={300}
-                />
-              </div>
+              <Link href={`/product/${product.slug}`}>
+                <div className="aspect-square w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:h-80">
+                  <Image
+                    src={product.imageUrl}
+                    alt="Product image"
+                    className="w-full h-full object-cover object-center lg:h-full lg:w-full"
+                    width={300}
+                    height={300}
+                  />
+                </div>
+              </Link>
 
               <div className="mt-4 flex justify-between">
                 <div>
